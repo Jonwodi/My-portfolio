1ff06694e939b5757be9f28d2b62ac5045862faa
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2022-03-13 20:03:02 +0000

    updated cv added

---
+++ js/scripts-bk.js
@@ -0,0 +1,64 @@
+var start = 0;
+var time = 3000;
+
+let codehanceImages = ['images/codehance1.png', 'images/codehance2.png', 'images/codehance3.png', 'images/codehance4.png'];
+let jobPlus1_Images = ['images/jobplus-v1-1.png', 'images/jobplus-v1-2.png', 'images/jobplus-v1-3.png', 'images/jobplus-v1-4.png'];
+let ldnFitnessImages = ['images/ldn-fitness1.png', 'images/ldn-fitness2.png', 'images/ldn-fitness3.png', 'images/ldn-fitness4.png', 'images/ldn-fitness5.png', 'images/ldn-fitness6.png'];
+let restaurantImages = ['images/restuarant-booking1.png', 'images/restuarant-booking2.png', 'images/restuarant-booking3.png', 'images/restuarant-booking4.png', 'images/restuarant-booking5.png'];
+
+let changeCodehanceImg = () => {
+  document.slide.src = codehanceImages[start];
+
+  if (start < codehanceImages.length - 1) {
+    start++;
+  } else {
+    start = 0;
+  }
+
+  setTimeout("changeCodehanceImg()", time);
+}
+
+let changejobPlusVol1Img = () => {
+  document.jobplus1.src = jobPlus1_Images[start];
+
+  if (start < jobPlus1_Images.length - 1) {
+    start++;
+  } else {
+    start = 0;
+  }
+
+  setTimeout("changejobPlusVol1Img()", time);
+}
+
+let = changeLdnFitnessImg = () => {
+  document.ldn_slideshow.src = ldnFitnessImages[start];
+
+  if (start < ldnFitnessImages.length - 1) {
+    start++;
+  } else {
+    start = 0;
+  }
+
+  setTimeout("changeLdnFitnessImg()", time);
+
+}
+
+let changeRestaurantImg = () => {
+  document.restaurant_slideshow.src = restaurantImages[start];
+
+  if (start < restaurantImages.length - 1) {
+    start++;
+  } else {
+    start = 0;
+  }
+
+  setTimeout("changeRestaurantImg()", time);
+
+}
+
+window.onload = function() {
+  changeCodehanceImg();
+  changejobPlusVol1Img();
+  changeLdnFitnessImg();
+  changeRestaurantImg();
+}
--- js/scripts.js
+++ js/scripts.js
@@ -1,69 +1,131 @@
 var start = 0;
+
 var time = 3000;
 
-let codehanceImages = ['images/codehance1.png', 'images/codehance2.png', 'images/codehance3.png', 'images/codehance4.png'];
-let jobPlus1_Images = ['images/jobplus-v1-1.png', 'images/jobplus-v1-2.png', 'images/jobplus-v1-3.png', 'images/jobplus-v1-4.png'];
-let ldnFitnessImages = ['images/ldn-fitness1.png', 'images/ldn-fitness2.png', 'images/ldn-fitness3.png', 'images/ldn-fitness4.png', 'images/ldn-fitness5.png', 'images/ldn-fitness6.png'];
-let restaurantImages = ['images/restuarant-booking1.png', 'images/restuarant-booking2.png', 'images/restuarant-booking3.png', 'images/restuarant-booking4.png', 'images/restuarant-booking5.png'];
+let images = [
+  ['images/codehance1.png', 'images/codehance2.png', 'images/codehance3.png', 'images/codehance4.png'],
+  ['images/csv-film-reader1.png', 'images/csv-film-reader2.png']
+  ['images/ldn-fitness1.png', 'images/ldn-fitness2.png', 'images/ldn-fitness3.png', 'images/ldn-fitness4.png', 'images/ldn-fitness5.png', 'images/ldn-fitness6.png'],
+  ['images/jobplus-v1-1.png', 'images/jobplus-v1-2.png', 'images/jobplus-v1-3.png', 'images/jobplus-v1-4.png'],
+  ['images/jobplus-v2-1.png', 'images/jobplus-v2-2.png', 'images/jobplus-v2-3.png', 'images/jobplus-v2-4.png'],
+  ['images/restuarant-booking1.png', 'images/restuarant-booking2.png', 'images/restuarant-booking3.png', 'images/restuarant-booking4.png', 'images/restuarant-booking5.png'],];
 
+function changeImg() {
+  let codehance = () => {
+    document.slide1.src = images[0][start];
 
-
-// function changeCodehanceImg() {
-//   document.slide.src = codehanceImages[start];
-
-//   if (start < codehanceImages.length - 1) {
-//     start++;
-//   } else {
-//     start = 0;
-//   }
-
-//   setTimeout("changeCodehanceImg()", time);
-
-// }
-
-// window.onload = changeCodehanceImg;
-
-// function changejobPlusVol1Img() {
-//   document.jobplus1.src = jobPlus1_Images[start];
-
-//   if (start < jobPlus1_Images.length - 1) {
-//     start++;
-//   } else {
-//     start = 0;
-//   }
-
-//   setTimeout("changejobPlusVol1Img()", time);
-
-// }
-
-// window.onload = changejobPlusVol1Img;
-
-// function changeLdnFitnessImg() {
-//   document.ldn_slideshow.src = ldnFitnessImages[start];
-
-//   if (start < ldnFitnessImages.length - 1) {
-//     start++;
-//   } else {
-//     start = 0;
-//   }
-
-//   setTimeout("changeLdnFitnessImg()", time);
-
-// }
-
-// window.onload = changeLdnFitnessImg();
-
-function changeRestaurantImg() {
-  document.restaurant_slideshow.src = restaurantImages[start];
-
-  if (start < restaurantImages.length - 1) {
-    start++;
-  } else {
-    start = 0;
+    if (start < images[0].length - 1) {
+      start++;
+    } else {
+      start = 0;
+    }
+    
+    setTimeout(codehance,2000);
   }
 
-  setTimeout("changeRestaurantImg()", time);
+  let filmReader = () => {
+    document.slide2.src = images[1][start];
 
+    if (start < images[1].length - 1) {
+      start++;
+    } else {
+      start = 0;
+    }
+
+    // setTimeout(filmReader, 3000);
+  }
+
+  let ldnFitness = () => {
+    document.slide3.src = images[2][start];
+
+    if (start < images[2].length - 1) {
+      start++;
+    } else {
+      start = 0;
+    }
+
+    setTimeout(ldnFitness, 2000);
+  }
+
+  let jobPlusVol1 = () => {
+    document.slide4.src = images[3][start];
+
+    if (start < images[3].length - 1) {
+      start++;
+    } else {
+      start = 0;
+    }
+
+    setTimeout(jobPlusVol1, 3000);
+  }
+
+  let jobPlusVol2 = () => {
+    document.slide5.src = images[4][start];
+
+    if (start < images[4].length - 1) {
+      start++;
+    } else {
+      start = 0;
+    }
+
+    setTimeout(jobPlusVol2, 3000);
+  }
+
+  let restaurant = () => {
+    document.slide6.src = images[5][start];
+
+    if (start < images[5].length - 1) {
+      start++;
+    } else {
+      start = 0;
+    }
+
+    setTimeout(restaurant, 3000);
+  }
+
+  function slideShow1() {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(codehance())
+        });
+      });
+    }
+
+  function slideShow3() {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(ldnFitness())
+        }, 16000);
+      });
+    }
+
+  (async () => {
+    try {
+      await slideShow1();
+      // await slideShow3();
+    } catch (err) {
+      console.log(err);
+    }
+  })();
+ 
+
+  // codehance();
+  // filmReader();
+  // ldnFitness();
+  // jobPlusVol1();
+  // jobPlusVol2();
+  // restaurant();
 }
 
-window.onload = changeRestaurantImg();
+
+
+
+
+// $slider1.on("mouseover", changeImg () {
+//   codehance();
+// });
+
+$(function() {
+  changeImg();
+});
+  
